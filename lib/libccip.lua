87617a5d1686dbedef0be88574e11c3635ed4ba2
Waoweens <30313089+Waoweens@users.noreply.github.com>
2022-10-01 09:31:47 +0700

    header version broken
    Signed-off-by: Waoweens <30313089+Waoweens@users.noreply.github.com>

--- lib/libccip.lua
+++
@@ -1,9 +0,0 @@
--- CC-IP Library (libccip)
--- Header File
-
-package.path = package.path .. ";/lib/cc-ip"
-
-require("libccip-common")
-require("libccip-client")
--- dofile("/lib/cc-ip/libccip-server.lua")
--- dofile("/lib/cc-ip/libccip-dns.lua")
